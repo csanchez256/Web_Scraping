d859254f8d2b1e14a0a2e184d9f0d220c7b71215
chris <css7c3@gmail.com>
2024-09-17 18:44:23 -0600

    html parser

--- webscraping.py
+++ webscraping.py
@@ -1,3 +1,38 @@
 # web scraping script
 
-# 
+# Making a GET request
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+
+#from urllib import urlopen
+
+"""
+text = urlopen('https://commonlyusedwords.com/2000-most-common-Spanish-words/').read()
+soup = BeautifulSoup(text)
+#print(soup)
+spanish = soup.select('td')
+"""
+
+def has_td_siblings(x):
+    return x.find_next_sibling().name == 'td'
+
+r = requests.get('https://commonlyusedwords.com/2000-most-common-Spanish-words/')
+soup = bs(r.text, 'html.parser')
+
+spanish_vocab = soup.select('tbody')
+
+for element in spanish_vocab:
+    if has_td_siblings(element):
+        print('yes')
+    else: 
+        print ('no')
+
+
+
+ 
+
+
+
+
+
